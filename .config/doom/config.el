d77141066782239c2b46cf858853c555b73f01af
Dorian Stoll <dorian.stoll@tmsp.io>
2020-11-25 17:15:43 +0100

    Emacs wrangling for copy / paste and local config
    Signed-off-by: Dorian Stoll <dorian.stoll@tmsp.io>

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -1,57 +1,22 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
+(setq user-full-name "Dorian Stoll")
+(setq user-mail-address "dorian.stoll@tmsp.io")
 
+(load! "local.el")
 
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
-(setq user-full-name "Dorian Stoll"
-      user-mail-address "dorian.stoll@tmsp.io")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-vibrant)
-(setq doom-font (font-spec :family "Fira Code" :size 13 :weight 'semi-light))
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
+(setq-default show-trailing-whitespace t)
+(setq-default indent-tabs-mode t)
 
+(cua-mode t)
+(transient-mark-mode 1)
+(setq cua-auto-tabify-rectangles nil)
+(setq cua-keep-region-after-copy t)
 
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
+(c-add-style "linux-custom"
+	'("linux"
+		 (c-offsets-alist
+			 (arglist-cont-nonempty . ++))))
 
-(setq show-trailing-whitespace t)
+(setq c-default-style "linux-custom")
